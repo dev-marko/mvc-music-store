d78588350c35040170e60f8992148dad7b5a820d
dev-marko <markospasenovski00@gmail.com>
2021-05-06 14:41:14 +0200

    Added the MusicStoreMVC project which was created with scaffolding

---
+++ MusicStoreMVCScaffolding/MusicStoreMVCScaffolding/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MusicStoreMVCScaffolding.MvcApplication" Language="C#" %>
---
+++ MusicStoreMVCScaffolding/MusicStoreMVCScaffolding/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MusicStoreMVCScaffolding.Startup))]
+namespace MusicStoreMVCScaffolding
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
